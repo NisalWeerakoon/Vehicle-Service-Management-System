86aa49db34fc56afabc57b7beef797e1e409ebbb
IT24103441 <it24103441@my.sliit.lk>
2026-08-26 14:10:36 +0530

    feat: add CustomWebApplicationFactory to support integration testing with in-memory database

--- tests/CustomerBookingService.IntegrationTests/CustomWebApplicationFactory.cs
+++ tests/CustomerBookingService.IntegrationTests/CustomWebApplicationFactory.cs
@@ -1,4 +1,5 @@
 using CustomerBookingService.Data;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
