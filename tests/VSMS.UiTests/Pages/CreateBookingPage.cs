0ce7f5f8973fccc4ac3e8558f069f94521060c71
IT24103441 <it24103441@my.sliit.lk>
2026-08-26 15:05:23 +0530

    feat: initialize UI testing project with Selenium and implement CreateBookingPage object model

--- tests/VSMS.UiTests/Pages/CreateBookingPage.cs
+++ tests/VSMS.UiTests/Pages/CreateBookingPage.cs
@@ -27,8 +27,14 @@ public class CreateBookingPage
 
     public void NavigateTo() => _driver.Navigate().GoToUrl($"{DriverFactory.BaseUrl}/bookings/create");
 
-    public bool HasNoVehicleEmptyState() =>
-        _driver.FindElements(By.CssSelector(".empty-state")).Any();
+    public bool HasNoVehicleEmptyState()
+    {
+        _wait.Until(d =>
+            d.FindElements(By.CssSelector(".empty-state")).Count > 0 ||
+            d.FindElements(By.Id("vehicleId")).Count > 0);
+
+        return _driver.FindElements(By.CssSelector(".empty-state")).Count > 0;
+    }
 
     /// <param name="vehicleIndex">
     /// Which &lt;option&gt; to pick, 1-based, skipping the "Choose your vehicle" placeholder.
@@ -36,10 +42,30 @@ public class CreateBookingPage
     public void CreateBooking(int vehicleIndex, string isoDate, string problemDescription)
     {
         _wait.Until(d => d.FindElement(By.Id("vehicleId")));
+        _wait.Until(d => new SelectElement(d.FindElement(By.Id("vehicleId"))).Options.Count > vehicleIndex);
 
-        new SelectElement(VehicleSelect).SelectByIndex(vehicleIndex);
-        DateInput.Clear();
-        DateInput.SendKeys(isoDate); // yyyy-MM-dd, matches the <input type="date">
+        var js = (IJavaScriptExecutor)_driver;
+
+        // Set select value via React native property setter
+        js.ExecuteScript(@"
+            var select = arguments[0];
+            var index = arguments[1];
+            var val = select.options[index].value;
+            var nativeSetter = Object.getOwnPropertyDescriptor(window.HTMLSelectElement.prototype, 'value').set;
+            nativeSetter.call(select, val);
+            select.dispatchEvent(new Event('change', { bubbles: true }));
+        ", VehicleSelect, vehicleIndex);
+
+        // Set date input value via React native property setter
+        js.ExecuteScript(@"
+            var input = arguments[0];
+            var val = arguments[1];
+            var nativeSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
+            nativeSetter.call(input, val);
+            input.dispatchEvent(new Event('input', { bubbles: true }));
+            input.dispatchEvent(new Event('change', { bubbles: true }));
+        ", DateInput, isoDate);
+
         ProblemTextarea.Clear();
         ProblemTextarea.SendKeys(problemDescription);
 
